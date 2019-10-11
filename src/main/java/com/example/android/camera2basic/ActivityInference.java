dc7fed9668961e06c81e01d560dd1e7e6b685a4e
steven <steven@kaist.ac.kr>
2019-10-11 16:48:29 +0900

    SH app develop

---
+++ src/main/java/com/example/android/camera2basic/ActivityInference.java
@@ -0,0 +1,154 @@
+package com.example.android.camera2basic;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.os.Trace;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Vector;
+import org.tensorflow.Operation;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+/** A classifier specialized to label images using TensorFlow. */
+public class ActivityInference implements Classifier {
+    private static final String TAG = "ActivityInference";
+
+    // Only return this many results with at least this confidence.
+    private static final int MAX_RESULTS = 3;
+    private static final float THRESHOLD = 0.1f;
+
+    // Config values.
+    private String inputName;
+    private String outputName;
+    private int inputSize;
+    private int imageMean;
+    private float imageStd;
+
+    // Pre-allocated buffers.
+    private Vector<String> labels = new Vector<String>();
+    private int[] intValues;
+    private float[] floatValues;
+    private float[] outputs;
+    private String[] outputNames;
+
+    private boolean logStats = false;
+
+    private TensorFlowInferenceInterface inferenceInterface;
+
+    private ActivityInference() {}
+
+    /**
+     * Initializes a native TensorFlow session for classifying images.
+     *
+     * @param assetManager The asset manager to be used to load assets.
+     * @param modelFilename The filepath of the model GraphDef protocol buffer.
+     * @param inputSize The input size. A square image of inputSize x inputSize is assumed.
+     * @param imageMean The assumed mean of the image values.
+     * @param imageStd The assumed std of the image values.
+     * @param inputName The label of the image input node.
+     * @param outputName The label of the output node.
+     * @throws IOException
+     */
+    public static Classifier create(
+            AssetManager assetManager,
+            String modelFilename,
+            int inputSize,
+            int imageMean,
+            float imageStd,
+            String inputName,
+            String outputName) {
+        ActivityInference c = new ActivityInference();
+        c.inputName = inputName;
+        c.outputName = outputName;
+
+        // Read the label names into memory.
+        // TODO(andrewharp): make this handle non-assets.
+        String actualFilename = "0819.pb";
+        Log.i(TAG, "Reading labels from: " + actualFilename);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(assetManager.open(actualFilename)));
+            String line;
+            while ((line = br.readLine()) != null) {
+                c.labels.add(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Problem reading label file!" , e);
+        }
+
+        c.inferenceInterface = new TensorFlowInferenceInterface(assetManager, modelFilename);
+
+        // The shape of the output is [N, NUM_CLASSES], where N is the batch size.
+        final Operation operation = c.inferenceInterface.graphOperation(outputName);
+        Log.i(TAG, "Read " + c.labels.size() + " labels, output layer size is " + 2);
+
+        // Ideally, inputSize could have been retrieved from the shape of the input operation.  Alas,
+        // the placeholder node for input in the graphdef typically used does not specify a shape, so it
+        // must be passed in as a parameter.
+        c.inputSize = inputSize;
+        c.imageMean = imageMean;
+        c.imageStd = imageStd;
+
+        // Pre-allocate buffers.
+        c.outputNames = new String[] {outputName};
+        c.intValues = new int[inputSize * (inputSize+1) * 3 / 4];
+        c.floatValues = new float[inputSize * (inputSize+1) * 3 / 4 * 3];
+        c.outputs = new float[2];
+
+        return c;
+    }
+
+    @Override
+    public float[] recognizeImage(final Bitmap bitmap) {
+        // Log this method so that it can be analyzed with systrace.
+        Trace.beginSection("recognizeImage");
+
+        Trace.beginSection("preprocessBitmap");
+        // Preprocess the image data from 0-255 int to normalized float based
+        // on the provided parameters.
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (int i = 0; i < intValues.length; ++i) {
+            final int val = intValues[i];
+            floatValues[i * 3 + 0] = (((val >> 16) & 0xFF) - imageMean) / imageStd;
+            floatValues[i * 3 + 1] = (((val >> 8) & 0xFF) - imageMean) / imageStd;
+            floatValues[i * 3 + 2] = ((val & 0xFF) - imageMean) / imageStd;
+        }
+        Trace.endSection();
+        // Copy the input data into TensorFlow.
+        Trace.beginSection("feed");
+        //inferenceInterface.feed(inputName, floatValues, inputSize);
+        inferenceInterface.feed(inputName, floatValues, 1, (inputSize+1)*3/4, inputSize, 3);
+        Trace.endSection();
+
+        // Run the inference call.
+        Trace.beginSection("run");
+        inferenceInterface.run(outputNames, logStats);
+        Trace.endSection();
+
+        // Copy the output Tensor back into the output array.
+        Trace.beginSection("fetch");
+        inferenceInterface.fetch(outputName, outputs);
+        Trace.endSection();
+
+        // Find the best classifications.
+        return outputs;
+    }
+
+    @Override
+    public void enableStatLogging(boolean logStats) {
+        this.logStats = logStats;
+    }
+
+    @Override
+    public String getStatString() {
+        return inferenceInterface.getStatString();
+    }
+
+    @Override
+    public void close() {
+        inferenceInterface.close();
+    }
+}
